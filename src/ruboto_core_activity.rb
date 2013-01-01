9d02b4b60e2d67120aa5028b3b3a5b9cb9997f16
Uwe Kubosch <uwe@kubosch.no>
2013-01-01 23:46:20 +0100

    * Added hopefully helpful text to the main activity and buttons to go to the home page or exit the app.

--- src/ruboto_core_activity.rb
+++ src/ruboto_core_activity.rb
@@ -1,20 +1,26 @@
 require 'ruboto/widget'
-require 'ruboto/util/toast'
 
 ruboto_import_widgets :Button, :LinearLayout, :TextView
-
-# http://xkcd.com/378/
 
 class RubotoCoreActivity
   def on_create(bundle)
     super
-    set_title 'Domo arigato, Mr Ruboto!'
+    set_title 'Ruboto - Ruby on Android'
 
     self.content_view =
-        linear_layout :orientation => :vertical do
-          @text_view = text_view :text => 'What hath Matz wrought?', :id => 42, :width => :match_parent,
-                                 :gravity => :center, :text_size => 48.0
-          button :text => 'M-x butterfly', :width => :match_parent, :id => 43, :on_click_listener => proc { butterfly }
+        linear_layout :orientation => :vertical, :background_color => 0x7FB90706 do
+          text_view :text => 'Welcome to Ruboto', :width => :match_parent,
+                    :gravity => :center, :text_size => 48.0, :background_color => 0x7F24456A
+          text_view :text => 'This app contains the common parts of Ruboto apps.
+You have probably come here after installing a Ruboto based app.
+You do not have to run this app to use Ruboto.  It will automatically be
+utilized by Ruboto apps that do not already include JRuby.  To continue,
+just select "Exit" and navigate to the app you installed.  It will start normally
+from now on.
+'.gsub(/\n+/, '  '),
+                    :width => :match_parent, :gravity => :center, :text_size => 24.0
+          button :text => 'Ruboto Home Page', :width => :match_parent, :on_click_listener => proc { go_home }
+          button :text => 'Exit', :width => :match_parent, :on_click_listener => proc { finish }
         end
   rescue
     puts "Exception creating activity: #{$!}"
@@ -23,9 +29,9 @@ class RubotoCoreActivity
 
   private
 
-  def butterfly
-    @text_view.text = 'What hath Matz wrought!'
-    toast 'Flipped a bit via butterfly'
+  def go_home
+    startActivity android.content.Intent.new android.content.Intent::ACTION_VIEW,
+                                             android.net.Uri.parse('http://ruboto.org/')
   end
 
 end

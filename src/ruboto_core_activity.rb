34012d6d0000f2f369ef92914489191b7f4c56df
Uwe Kubosch <uwe@kubosch.no>
2013-05-06 00:49:13 +0200

    * Updated to Ruboto 0.11.0 * Allow running in Ruby 2.0 mode * Updated packaged ARJDBC to version 1.2.9 * Changed to use HTTPS when fetching gems. * Put RubotoCore releases on the public website * Open the installing app upon open.

--- src/ruboto_core_activity.rb
+++ src/ruboto_core_activity.rb
@@ -7,8 +7,10 @@ class RubotoCoreActivity
   def onCreate(bundle)
     super
     set_title R.string.app_title
-    installerPackageName = getIntent().getStringExtra(
-                Intent.EXTRA_INSTALLER_PACKAGE_NAME)
+    installerPackageName = getIntent().getStringExtra(Intent::EXTRA_INSTALLER_PACKAGE_NAME)
+    pm_installer_package_name = package_manager.getInstallerPackageName($package_name)
+    p installerPackageName
+    p pm_installer_package_name
 
     self.content_view =
         linear_layout :orientation => :vertical, :background_color => 0xFFB90706 do
@@ -16,8 +18,8 @@ class RubotoCoreActivity
                     :gravity => :center, :text_size => 48.0, :background_color => 0xFF24456A
           text_view(:text => installerPackageName,
                     :width => :match_parent, :gravity => :center, :text_size => 24.0) if installerPackageName
-          text_view(:text => package_manager.getInstallerPackageName($package_name),
-                    :width => :match_parent, :gravity => :center, :text_size => 24.0) if package_manager.getInstallerPackageName($package_name)
+          text_view(:text => pm_installer_package_name,
+                    :width => :match_parent, :gravity => :center, :text_size => 24.0) if pm_installer_package_name
           text_view :text => getString(R.string.content).gsub(/\n+/, '  '),
                     :width => :match_parent, :gravity => :center, :text_size => 24.0
           button :text => getString(R.string.homepage), :width => :match_parent, :on_click_listener => proc { go_home }
